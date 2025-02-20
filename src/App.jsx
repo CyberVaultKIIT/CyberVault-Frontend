a57784c39b7051164c74794eb666b2bf1313b9cc
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-20 15:43:24 +0530

    About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import Navbar from './components/navbar/Navbar.jsx';
-import Footer from './components/Footer';
-import NotFound from "./components/NotFound/notFound";
-import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
-import Home from './pages/home/home.jsx';
+import React from 'react'
+// import Navbar from './components/Navbar/Navbar.jsx'
+import Footer from './components/Footer'
+import NotFound from './components/NotFound/notFound'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom'
+import Home from './pages/home/home.jsx'
+import About from './pages/about/about.jsx'
 const Layout = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Outlet />
       {/* <Footer /> */}
     </div>
@@ -19,20 +25,14 @@ function App() {
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={
-            <Home />
-          }/>
-          <Route path="/team" element={
-            <NotFound />
-          }/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<NotFound />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
-
-
+export default App
