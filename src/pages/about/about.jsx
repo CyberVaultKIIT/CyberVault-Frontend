74e3b7aac77cc8e5e05cfd165f455768e1ddab6d
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-02-21 21:22:19 +0530

    Check

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import styles from './about.module.scss'
-import founderImage from '../../assets/images/founder.svg'
-import Logo from '../../assets/images/cybervault-logo.svg'
-import Kiit from '../../assets/images/kiit.svg'
 
-function About() {
+function App() {
   const cards = [
     {
-      image: Logo,
+      image: 'cybervault-logo.svg',
       title: 'CYBERVAULT',
       content:
         ' Hello everyone! Welcome to CyberVault, the premier cybersecurity society at KIIT Deemed To Be University. We are a vibrant and dynamic community of innovators, learners, and cybersecurity enthusiasts, dedicated to advancing the field of cybersecurity Our society is committed to fostering an environment of continuous learning and innovation. We offer a diverse range of activities to enhance your skills and understanding of the cybersecurity landscape.',
     },
     {
-      image: Kiit,
+      image: 'kiit.svg',
       title: 'KIIT',
       content:
         ' KIIT Bhubaneswar, officially known as Kalinga Institute of Industrial Technology, is one of India’s premier educational institutions. Established in 1992 by Dr. Achyuta Samanta, KIIT has grown exponentially over the years, becoming a Deemed to be University in 2004. Located in the capital city of Odisha, KIIT is renowned for its world-class infrastructure, diverse academic programs, and a strong emphasis on research and innovation.',
@@ -34,7 +31,7 @@ function About() {
       <div className={styles.main}>
         <section className={styles.founderSection}>
           <img
-            src={founderImage}
+            src="founder.svg"
             alt="Founder"
             className={styles.founderImage}
           />
@@ -69,4 +66,4 @@ function About() {
   )
 }
 
-export default About
+export default App
